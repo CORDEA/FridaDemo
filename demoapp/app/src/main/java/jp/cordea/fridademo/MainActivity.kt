704c4845d7b351683914a13ffb04bd3bf5d805fa
Yoshihiro Tanaka <contact@cordea.jp>
2020-01-19 17:37:58 +0900

    use view binding

--- demoapp/app/src/main/java/jp/cordea/fridademo/MainActivity.kt
+++ demoapp/app/src/main/java/jp/cordea/fridademo/MainActivity.kt
@@ -1,21 +1,21 @@
 package jp.cordea.fridademo
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import jp.cordea.fridademo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
-        fab.setOnClickListener { view ->
+        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
