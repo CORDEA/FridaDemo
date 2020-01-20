4c3ff38038f64aa9f9c61ae038886b15c4aedad9
Yoshihiro Tanaka <contact@cordea.jp>
2020-01-20 23:45:53 +0900

    show toast

--- demoapp/app/src/main/java/jp/cordea/fridademo/MainActivity.kt
+++ demoapp/app/src/main/java/jp/cordea/fridademo/MainActivity.kt
@@ -3,11 +3,13 @@ package jp.cordea.fridademo
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import jp.cordea.fridademo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val detector = Detector()
     private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +18,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
-        binding.fab.setOnClickListener { view ->
+        binding.fab.setOnClickListener {
             ++count
             binding.content.textView.text = count.toString()
         }
         binding.content.textView.text = count.toString()
+
+        binding.content.button.setOnClickListener {
+            if (detector.detect()) {
+                Toast.makeText(this, "DO NOT SHOW THIS TOAST!!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
